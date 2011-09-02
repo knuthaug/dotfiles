c6577227a58cafc906e23e8cebe231ad52e6f63c
Knut Haugen <knuthaug@localhost>
2011-09-02 09:17:28 +0200

    added new file for loopkin

---
+++ emacs/machines/loopkin.el
@@ -0,0 +1,13 @@
+(set-background-color "black")
+(set-foreground-color "white")
+
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
+
+
+(provide 'loopkin)
+
