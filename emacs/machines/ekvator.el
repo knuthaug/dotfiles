410326df45e68ccd63c8015ddf6d2d597952e295
Knut Haugen <knut.haugen@amedia.no>
2013-11-12 15:37:07 +0100

    custom file for machine ekvator

---
+++ emacs/machines/ekvator.el
@@ -0,0 +1,15 @@
+
+(set-background-color "black")
+(set-foreground-color "white")
+;(global-set-key "M-8" "{")
+
+(custom-set-variables
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 190 t)
+ '(ecb-options-version "2.40")
+ '(mac-option-modifier nil)
+ '(mac-function-modifier 'meta)
+ '(default-frame-alist (quote ((tool-bar-lines . 0) (menu-bar-lines . 1) (foreground-color . "Black") (background-color . "White") (cursor-type . box) (cursor-color . "Red") (vertical-scroll-bars) (internal-border-width . 0) (fringe))))
+)
+
+(provide 'ekvator)
